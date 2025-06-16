af2bc6aef78e79c7e4d9f21a70ed4fef8e6280c3
laboratorio <laboratorio@UPOAULA10707>
2025-06-16 07:54:57 -0500

    Agregar archivos de proyecto.

---
+++ ActualizacionTallerApuntes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ActualizacionTallerApuntes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ActualizacionTallerApuntes/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ActualizacionTallerApuntes;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
